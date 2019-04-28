113e71c7ee76732699899b7aec7789e446b33dca
hariudkmr <hariudkmr@gmail.com>
2019-04-29 03:06:53 +0800

    gmock part2 - invoke

--- test/testdb.cpp
+++ test/testdb.cpp
@@ -8,10 +8,12 @@ using namespace std;
 using ::testing::AtLeast;
 using ::testing::Return;
 using ::testing::_;
+using ::testing::Invoke;
+using ::testing::InvokeWithoutArgs;
 
 class DataBaseConnect {
     public:
-        virtual bool login(string username, string password){return true;}
+        virtual bool login(string username, string password){cout<< "Original Login\n" ; return true;}
         virtual bool logout(string username) {return true;}
         virtual int fetchRecord() {return -1;}
 };
@@ -23,6 +25,14 @@ class MockDB : public DataBaseConnect {
         MOCK_METHOD2(login, bool(string username, string password));
 };
 
+class MockDataBaseConnect {
+    public:
+        MOCK_METHOD0(fetchRecord, int());
+        MOCK_METHOD1(logout, bool(string username));
+        MOCK_METHOD2(login, bool(string username, string password));
+};
+
+
 class MyDatabase {
     DataBaseConnect & dbC;
     public:
@@ -31,6 +41,23 @@ class MyDatabase {
         int Init(string username, string password){
             if(dbC.login(username,password) != true) {
               if(dbC.login(username,password) != true) {  
+                cout << "DB Failure 2nd Time\n"; return -1;
+                } 
+            } else {
+                cout << "DB Success\n"; return 1;
+            }
+        }
+};
+
+
+class MyDatabase2 {
+    MockDataBaseConnect & mdbC;
+    public:
+        MyDatabase2(MockDataBaseConnect & _mdbC) : mdbC(_mdbC) {}
+
+        int Init(string username, string password){
+            if(mdbC.login(username,password) != true) {
+              if(mdbC.login(username,password) != true) {  
                 cout << "DB Failure 2nd Time\n"; return -1;
                 } 
             } else {
@@ -80,6 +107,90 @@ TEST(MyDBTest, LoginOnCall)
     MyDatabase db(mdb);
 
     ON_CALL(mdb, login(_,_)).WillByDefault(Return(true));
+
+    //ACT
+    int retvalue = db.Init("Hari", "SimplePass");
+
+    //Assert
+    EXPECT_EQ(retvalue,1);
+}
+
+TEST(MyDBTest, InvokeWithClass)
+{
+    MockDataBaseConnect mdbc;
+    MyDatabase2 db2(mdbc);
+
+    EXPECT_CALL(mdbc, login("Hari", "SimplePass"))
+    .Times(1)
+    .WillOnce(Return(true));
+
+    //ACT
+    int retvalue = db2.Init("Hari", "SimplePass");
+
+    //Assert
+    EXPECT_EQ(retvalue,1);
+}
+
+TEST(MyDBTest, InvokeClass)
+{
+    //Arrange
+    MockDB mdb;
+    MyDatabase db(mdb);
+    DataBaseConnect dbtest;
+
+    EXPECT_CALL(mdb, login("Hari", "SimplePass"))
+    .Times(AtLeast(1))
+    .WillOnce(Invoke(&dbtest, &DataBaseConnect::login));
+
+    //ACT
+    int retvalue = db.Init("Hari", "SimplePass");
+
+    //Assert
+    EXPECT_EQ(retvalue,1);
+}
+
+struct testABC {
+    bool DummyLogin(string username, string password){
+        cout << "Dummy Function Called\n";
+        return true;
+    }
+
+};
+
+TEST(MyDBTest, InvokeStruct)
+{
+    //Arrange
+    MockDB mdb;
+    MyDatabase db(mdb);
+    testABC dbtest;
+
+    EXPECT_CALL(mdb, login("Hari", "SimplePass"))
+    .Times(AtLeast(1))
+    .WillOnce(Invoke(&dbtest, &testABC::DummyLogin));
+
+    //ACT
+    int retvalue = db.Init("Hari", "SimplePass");
+
+    //Assert
+    EXPECT_EQ(retvalue,1);
+}
+
+bool DummyFn()
+{
+    cout << "Global Function Called\n";
+    return true;
+}
+
+TEST(MyDBTest, InvokeGlobalFn)
+{
+    //Arrange
+    MockDB mdb;
+    MyDatabase db(mdb);
+
+
+    EXPECT_CALL(mdb, login("Hari", "SimplePass"))
+    .Times(AtLeast(1))
+    .WillOnce(InvokeWithoutArgs(DummyFn));
 
     //ACT
     int retvalue = db.Init("Hari", "SimplePass");
